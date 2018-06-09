7cfc07fecfeeffe0ab54960e2fc8a8be5b84fe15
Zack Ream <zbream@outlook.com>
2018-06-09 14:22:26 -0400

    build: update firebase to remove workaround

--- src/app/api-services/firebase/firebase-api-services.module.ts
+++ src/app/api-services/firebase/firebase-api-services.module.ts
@@ -2,7 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { FirebaseAppConfig, FirebaseOptions } from '@firebase/app-types';
-import { AngularFireModule, FirebaseAppConfigToken, FirebaseOptionsToken } from 'angularfire2';
+import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { IssuesApiService, ProjectsApiService } from '../interfaces';
@@ -29,18 +29,13 @@ const FIREBASE_APP_CONFIG: FirebaseAppConfig = {
 @NgModule({
   imports: [
     HttpClientModule,
-    // TODO: remove workaround https://github.com/angular/angularfire2/issues/1635
-    // AngularFireModule.initializeApp(OPTIONS, APP_CONFIG),
-    AngularFireModule,
+    AngularFireModule.initializeApp(FIREBASE_OPTIONS, FIREBASE_APP_CONFIG),
     AngularFirestoreModule,
   ],
   providers: [
     { provide: FIREBASE_API, useValue: API },
     { provide: IssuesApiService, useClass: FirebaseIssuesApiService },
     { provide: ProjectsApiService, useClass: FirebaseProjectsApiService },
-    // TODO: remove workaround https://github.com/angular/angularfire2/issues/1635
-    { provide: FirebaseOptionsToken, useValue: FIREBASE_OPTIONS },
-    { provide: FirebaseAppConfigToken, useValue: FIREBASE_APP_CONFIG },
   ],
 })
 export class FirebaseApiServicesModule {}
